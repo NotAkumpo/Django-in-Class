a7f5c99af648d1a52add04469a4e8cc2177f0b3e
NotAkumpo <abdiel.evangelista@student.ateneo.edu>
2024-04-04 14:16:57 +0800

    Pre Lab 4

--- Django/tasksapp/tasksapp/settings.py
+++ Django/tasksapp/tasksapp/settings.py
@@ -118,7 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 LOGIN_REDIRECT_URL = '/tasks/list'
 LOGOUT_REDIRECT_URL = '/tasks/list'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
